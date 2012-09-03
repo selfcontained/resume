5ea5bf745934335a9f0092b1f89dee8af6233cd5
Brad Harris <bmharris@gmail.com>
2012-09-03 17:15:25 -0600

    removing logging

--- resources/socket.js
+++ resources/socket.js
@@ -1,17 +1,9 @@
 (function() {
 	var socket = io.connect('http://resume.selfcontained.us');
 
-	socket.on('connect', function(data) {
-		console.log('connected client', data);
-	});
 	socket.on('stats', function (data) {
-		console.log('stats fired: ', data);
 		$('#connections span').html(data.connections);
 		$('#memory span').html(data.memory+' mb');
 	});
 
-	$(window).on('beforeunload', function() {
-		console.log('test');
-		socket.disconnect();
-	});
 })();
