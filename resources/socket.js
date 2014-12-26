e504f1679c3e5dbb580a64b44786f6c49a4de0b1
Brad Harris <bmharris@gmail.com>
2014-12-25 22:54:49 -0700

    removing this old app

--- resources/socket.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var socket = io.connect('http://resume.selfcontained.us');
-
-	socket.on('stats', function (data) {
-		$('#connections span').html(data.connections);
-		$('#memory span').html(data.memory+' mb');
-	});
-
-})();
